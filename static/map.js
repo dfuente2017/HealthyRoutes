79e8991f1f981961e790a461a3747c80a9ee6e26
dfuente2017 <48331210+dfuente2017@users.noreply.github.com>
2021-12-04 19:43:19 +0100

    First version of geocoder working

--- static/map.js
+++ static/map.js
@@ -1,7 +1,8 @@
 const tilesProvider = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
 
-let mymap = L.map('myMap').setView([51.505,-0.09],13)
+let map = L.map('myMap').setView([51.505,-0.09],13)
 
 L.tileLayer(tilesProvider,{
     maxZoom: 18
-})
+}).addTo(map);
+L.Control.geocoder().addTo(map);
